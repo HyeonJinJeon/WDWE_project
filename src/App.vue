5cf28fdd671b543bd7112715840b87482fd1b58c
eunv <dmsql1091@naver.com>
2023-02-08 15:57:46 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,11 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 
-#app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  margin-top: 150px;
-  text-align: center;
-  color: #2c3e50;
-}
+//#app {
+//  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+//  margin-top: 150px;
+//  text-align: center;
+//  color: #2c3e50;
+//}
 </style>
