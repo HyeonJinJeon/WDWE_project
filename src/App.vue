1326588e25d099f1b126522fc5e009234b38676f
Hyeonjin Jeon <nd3807@naver.com>
2023-02-08 19:00:29 +0900

    초반 환경 설정

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+<script>
 
-#app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
-  margin-top: 150px;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,8 +7,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    }
+      name: 'login',
+      component: () => import('./views/Login'),
+    },
+    {
+      path: '/signUp',
+      name: 'SignUp',
+      component: () => import('./views/SignUp'),
+    },
   ]
 })
