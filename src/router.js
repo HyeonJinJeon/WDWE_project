b95710d6e25ece0878dc0a0923a6193695d3cd2c
eunv <dmsql1091@naver.com>
2023-02-08 18:50:43 +0900

    login,mainPg 라우터 설정

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -9,7 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      component: Login
-    }
+      component: () => import('./views/Login'),
+    },
+    {
+      path: '/mainPg',
+      name: 'mainPg',
+      component: () => import('./views/MainPg.vue'),
+    },
   ]
 })
