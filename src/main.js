ea61837b8b49b37aee9f4a1e3bb7cb9a79372710
eunv <dmsql1091@naver.com>
2023-02-09 11:28:29 +0900

    store 적용

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import vuetify from './plugins/vuetify'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import {firebase} from "@/firebase/firebaseConfig";
+import "firebase/auth";
+import store from "@/store";
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -13,8 +17,60 @@ export default new Router({
     },
     {
       path: '/signUp',
-      name: 'SignUp',
+      name: 'signUp',
       component: () => import('./views/SignUp'),
+    },
+    {
+      path: '/mainPg',
+      name: 'mainPg',
+      component: () => import('./views/MainPg.vue'),
+    },
+    {
+      path: '/groupSet',
+      name: 'groupSet',
+      component: () => import('./views/GroupSet.vue'),
     },
   ]
 })
+const makeTitle = (title) =>
+    title ? `${title} | WDWE` : "WDWE";
+
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    document.title = makeTitle(to.meta.title);
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from, next) => {
+  firebase.auth().onAuthStateChanged(async (user) => {
+
+    // let _isAdmin = '';
+    // let _isMaster = '';
+
+    await store.dispatch('getUser', user)
+        .then(() => {
+          if (user) {
+            console.log(user)
+            console.log('user', store.state.user)
+            // _isAdmin = store.state.claims.isAdmin
+            // console.log('isAdmin : ', _isAdmin)
+            // _isMaster = store.state.claims.isMaster
+            // console.log('isMaster : ', _isMaster)
+          } else {
+            console.log('없다.')
+          }
+        })
+
+
+    const firebaseCurrentUser = firebase.auth().currentUser
+    console.log(firebaseCurrentUser)
+
+    if (store.state.firebaseLoaded)
+      next()
+  })
+
+});
+
+
+export default router
