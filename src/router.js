16e3ab2931044ae092e6d6b61864f8789b0b2953
Hyeonjin Jeon <nd3807@naver.com>
2023-02-09 11:58:24 +0900

    추가

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -15,6 +14,11 @@ export default new Router({
       path: '/signUp',
       name: 'SignUp',
       component: () => import('./views/SignUp'),
+    },
+    {
+      path: '/mainPg',
+      name: 'MainPg',
+      component: () => import('./views/MainPg'),
     },
   ]
 })
