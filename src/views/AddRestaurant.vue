a509f64cae786d62ff1c0523fc358b8f04ece355
eunv <dmsql1091@naver.com>
2023-02-16 18:13:49 +0900

    주소등록

--- src/views/AddRestaurant.vue
+++ src/views/AddRestaurant.vue
@@ -4,7 +4,6 @@
             aria-hidden="true"></b-icon>
     <!--  <div class="bgImg">-->
     <!--    <img src="../assets/images/receipt.jpg">-->
-
     <div>
       <h3 style="position: absolute; top: 80px; left:350px; font-weight: 600; ">상호 등록</h3>
     </div>
@@ -31,6 +30,7 @@
           <option value="일식">일식</option>
           <option value="중식">중식</option>
           <option value="분식">분식</option>
+          <option value="카페">카페</option>
         </b-select>
       </div>
 
@@ -40,9 +40,8 @@
         <button style="white-space:nowrap; margin: 0 auto; background-color: #455a64; color: #FFFFFF" class="btn"
                 @click="searchGeo(resGeo)">이동
         </button>
-
       </div>
-
+      <h4>{{ address }}</h4>
 
       <!--------------------------------------------------------메뉴 추가는 나중에 구현---------------------------------------------------------------->
 
@@ -132,6 +131,7 @@ export default {
       resNum: "",
       resType: "",
       resGeo: "",
+      address: [],
     }
   },
   mounted() {
@@ -148,6 +148,7 @@ export default {
         type: self.resType,
         groupCode: localStorage.groupCode,
         geo: marker,
+        address: self.address,
       }
       db.collection('restaurant')
           .add(_data)
@@ -186,9 +187,24 @@ export default {
         // this.changeLatLng();
         this.lat = latlng.getLat();
         this.long = latlng.getLng();
-        console.log(this.map.relayout())
+        // console.log(this.map.relayout())
         console.log(this.lat)
+        self.address = getAddr(this.lat, this.long);
       });
+
+      function getAddr(lat, lng) {
+        let geocoder = new kakao.maps.services.Geocoder();
+        let coord = new kakao.maps.LatLng(lat, lng);
+        let callback = function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            const address = result[0].address.address_name
+            console.log(address)
+          }
+        }
+        geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+        const address = geocoder.coord2Address(coord.getLng(), coord.getLat(), callback)
+        console.log(address)
+      }
     },
     searchGeo(geo) {
       console.log(kakao)
