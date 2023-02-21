20683f41fb8d635e9a0d7eb5279eea7d6f775c44
Hyeonjin Jeon <nd3807@naver.com>
2023-02-21 16:00:53 +0900

    css 수정

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,34 +2,35 @@
   <v-app>
     <v-main>
       <div class="backgroundImg">
-        <!--        <div style="width: 750px; height: 100vh; margin: 0 auto">-->
-        <div class="center">
-          <div class="title">
-            <p>
-              <span style="font-size:100px;">W</span>
-              <span style="font-size:50px;">hat </span>
-              <span style="font-size:100px;">D</span>
-              <span style="font-size:50px;">o </span>
-              <span style="font-size:100px;">W</span>
-              <span style="font-size:50px;">e </span>
-              <span style="font-size:100px;">E</span>
-              <span style="font-size:50px;">at </span>
-              <i class="fas fa-utensils fa-3x"></i>
-            </p>
-          </div>
-
-          <div class="white-bg">
-            <p class="h4 text-center mb-4">Sign in</p>
-            <label for="idIn" class="grey-text">Your id</label>
-            <input type="text" id="idIn" class="form-control" v-model="id">
-            <br/>
-            <label for="pwIn" class="grey-text">Your password</label>
-            <input type="password" id="pwIn" class="form-control" v-model="pw" v-on:keypress.enter.prevent=login>
-            <div class="text-center mt-4">
-              <button class="btn btn-indigo" type="submit" @click="login" style="color: white;">Login</button>
-              <button class="btn btn-indigo" type="submit" @click="goSignUp" style="color: white;">sign up</button>
+        <div class="black-bg">
+          <div class="center">
+            <div class="title">
+              <p>
+                <span style="font-size:100px;">W</span>
+                <span style="font-size:50px;">hat </span>
+                <span style="font-size:100px;">D</span>
+                <span style="font-size:50px;">o </span>
+                <span style="font-size:100px;">W</span>
+                <span style="font-size:50px;">e </span>
+                <span style="font-size:100px;">E</span>
+                <span style="font-size:50px;">at </span>
+                <i class="fas fa-utensils fa-3x"></i>
+              </p>
             </div>
-            <!-- Default form login -->
+
+            <div class="white-bg">
+              <p class="h4 text-center mb-4">Sign in</p>
+              <label for="idIn" class="grey-text">Your id</label>
+              <input type="text" id="idIn" class="form-control" v-model="id">
+              <br/>
+              <label for="pwIn" class="grey-text">Your password</label>
+              <input type="password" id="pwIn" class="form-control" v-model="pw" v-on:keypress.enter.prevent=login>
+              <div class="text-center mt-4">
+                <button class="btn btn-indigo" type="submit" @click="login" style="color: white;">Login</button>
+                <button class="btn btn-indigo" type="submit" @click="goSignUp" style="color: white;">sign up</button>
+              </div>
+              <!-- Default form login -->
+            </div>
           </div>
         </div>
       </div>
@@ -107,24 +108,29 @@ export default {
 
 <style scoped>
 .backgroundImg {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../assets/images/startBg.jpg");
+  background-image: url("../assets/images/startBg.jpg");
+  background-color: rgba(0, 0, 0, 0.5);
   height: 100vh;
   width: 100%;
   background-size: cover;
   /*padding-top: 30vh;*/
 }
-
+.black-bg {
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 50px;
+}
 .white-bg {
-  /*opacity: 0.5;*/
-  max-width: 600px;
+  position: relative;
+  max-width: 400px;
   align: center;
   background: white;
   border-radius: 8px;
-  padding: 60px;
-  position: fixed;
+  padding: 50px;
   top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  /*left: 24vh;*/
   margin: 0 auto;
 }
 
@@ -135,17 +141,17 @@ a {
 }
 
 .title {
-  position: fixed;
-  top: 25%;
-  left: 35%;
-  /*left: 35%;*/
-  /*top: 10vh;*/
-  font-family: 'Roboto';
+  /*text-align: center;*/
+  /*padding-top: 10%;*/
   font-style: normal;
+  font-weight: 700;
+  font-size: 1000px;
+  line-height: 59px;
   color: #FFFFFF;
-  text-align: center;
+
   text-shadow: 0px 8px 4px rgba(0, 0, 0, 0.25);
 }
+
 
 .center {
   /*position: absolute;*/
